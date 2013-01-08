8861f540e73bfe27a36f7689d98398f5c442538f
w3magz <repo@w3magz.com>
2013-01-08 15:24:27 +0700

    add kota.php

---
+++ kota.php
@@ -0,0 +1,63 @@
+<?php
+/** 
+* Daftar kota di Indonesia
+* @contributor: hehe,hehe,hehe, (silakan masukkan nama agan disini)
+*/
+$indonesia = Array(
+	'provinsi' => Array(
+		'Nanggroe Aceh Darussalam' => Array('kota' => Array(
+										'Banda Aceh',
+										'Langsa',
+										'Lhokseumawe',
+										'Takengon',
+										'Tapaktuan',
+										'Meulaboh',
+										'Sigli',
+										'Kutacane',
+										'Singkil',
+										'Subulussalam'
+									)),
+		'Sumatera Utara'          => Array('kota' => Array(
+										'Medan',
+										'Binjai',
+										'Stabat',
+										'Tebing Tinggi'
+							
+		))
+	)
+);
+$output = isset($_GET['output']) ? $_GET['output'] : '';
+switch($output) :
+	case 'json':
+		header('Content-type: text/json');
+		header('Content-type: application/json');
+		echo json_encode($indonesia);
+	break;
+	
+	case 'xml':
+		//xml here
+	break;
+	
+	case 'sql':
+		//sql output here
+	break;
+	
+	case 'table':
+		//table output here
+	break;
+	
+	default :
+		echo '<pre>';
+		print_r($indonesia);
+		echo '</pre>';
+		echo '<hr>';
+	break;
+endswitch;
+if(isset($_GET['outp'])) {
+	
+}
+
+else {
+	
+}
+?>
